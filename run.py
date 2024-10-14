d5d426b867dba1503babea6f6d6f3825e7c9e803
Erin Doyle <erin.v.doyle@gmail.com>
2024-10-14 06:37:34 +0000

    add length of stay and date flexibility to get_trip_details()

--- run.py
+++ run.py
@@ -28,12 +28,35 @@ def get_trip_details():
     """
     Asks user for travel date, flexibility, and length of trip
     """
+    #get date
     travel_date_str = input("First things first, when would you like to depart? (Please enter a date in YYYY-MM-DD format, for example: 2025-09-27): ")
     try:
         travel_date = datetime.strptime(travel_date_str, "%Y-%m-%d")
     except ValueError:
         print("Oops. Please enter a date in YYYY-MM-DD format.")
         return None
+
+    #get flexibility
+    flexibility_response = input("Are you flexible with your date (+/- 1-3 days)? (yes/no): ").strip().lower()
+    if flexibility_response not in ['yes', 'no']:
+        print("Please answer with 'y' for yes or 'n' for no.")
+        return None
+
+    #Get length of stay
+    try:
+        length_of_stay = int(input("How many days do you plan to stay? "))
+    except ValueError:
+        print("Please enter a valid number for length of stay.")
+        return None
+
+    return {
+        'travel_date': travel_date,
+        'date_flexibility': date_flexibility,
+        'length_of_stay': length_of_stay
+    }
+
+
+    
 
 greeting()
 get_trip_details()
